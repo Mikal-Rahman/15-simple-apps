de6c37172fd59c4de0d9f20b657ed876ffb5392d
Mikal-Rahman <ibnmikal@gmail.com>
2023-07-22 13:42:54 +0700

    Committing changes to color-flipper before removal

---
+++ counter/app.js
@@ -0,0 +1,29 @@
+// here is the initial count value
+let count = 0;
+
+// here we select the value and the buttons
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach((btns) => {
+  btns.addEventListener("click", (e) => {
+    const styles = e.currentTarget.classList;
+    if (styles.contains("decrease")) {
+      count--;
+    } else if (styles.contains("increase")) {
+      count++;
+    } else {
+      count = 0;
+    }
+    if (count > 0) {
+      value.style.color = "green";
+    }
+    if (count < 0) {
+      value.style.color = "red";
+    }
+    if (count === 0) {
+      value.style.color = "black";
+    }
+    value.textContent = count;
+  });
+});
