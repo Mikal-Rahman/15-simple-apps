75e8bcdb3da70c5977c3c5cf871630e45e22f750
Mikal-Rahman <ibnmikal@gmail.com>
2023-07-23 19:33:38 +0700

    initial commit for review app

---
+++ README.md
@@ -0,0 +1,6 @@
+These simple yet cool javascript projects are meant to sharpen our programming skills by actually builing the projects out and understanding how they work.
+
+I added comments in these projects so I can come back to them as a reference. Years from now it will be nostalgic tp come back to them and look at how far I have come as well as how programming as a whole has progressed.
+
+Acknowledgments
+Special thanks to https://github.com/john-smilga for the step-by-step guide on creating this app.
---
+++ reviews/app.js
@@ -0,0 +1,86 @@
+// local reviews data
+const reviews = [
+  {
+    id: 1,
+    name: "susan smith",
+    job: "web developer",
+    img: "https://www.course-api.com/images/people/person-1.jpeg",
+    text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+  },
+  {
+    id: 2,
+    name: "anna johnson",
+    job: "web designer",
+    img: "https://www.course-api.com/images/people/person-2.jpeg",
+    text: "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
+  },
+  {
+    id: 3,
+    name: "peter jones",
+    job: "intern",
+    img: "https://www.course-api.com/images/people/person-4.jpeg",
+    text: "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
+  },
+  {
+    id: 4,
+    name: "bill anderson",
+    job: "the boss",
+    img: "https://www.course-api.com/images/people/person-3.jpeg",
+    text: "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
+  },
+];
+
+// here I am bringing my elements into my javascript
+const img = document.getElementById("person-img");
+const author = document.getElementById("author");
+const job = document.getElementById("job");
+const info = document.getElementById("info");
+
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+const randomBtn = document.querySelector(".random-btn");
+
+// since we are clicking through reviews we need a place to start from. below is our staring point and we will put the code further down that will perform that functionality
+let currentItem = 0;
+
+// this DOMContentLoaded business fires when the initial DOM content has been fully loaded and parsed. here we have an even listener sitting on the the window object
+window.addEventListener("DOMContentLoaded", () => {
+  showPerson(currentItem);
+});
+
+// show person based on the currentItem global variable
+
+function showPerson() {
+  const item = reviews[currentItem];
+  img.src = item.img;
+  author.textContent = item.name;
+  job.textContent = item.job;
+  info.textContent = item.text;
+}
+
+// show next person
+
+nextBtn.addEventListener("click", () => {
+  currentItem++;
+  if (currentItem > reviews.length - 1) {
+    currentItem = 0;
+  }
+  showPerson();
+});
+
+// show prev person
+
+prevBtn.addEventListener("click", () => {
+  currentItem--;
+  if (currentItem < 0) {
+    currentItem = reviews.length - 1;
+  }
+  showPerson();
+});
+
+//show random review and I think I did it by myself
+
+randomBtn.addEventListener("click", () => {
+  currentItem = Math.floor(Math.random() * reviews.length);
+  showPerson();
+});
